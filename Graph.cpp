f67fd0a07097b640dcb86a8d0f852c3aeaccfa61
AndreyKz <andreykz96@mail.ru>
2017-06-02 19:16:14 +0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -6,6 +6,31 @@
 using namespace std;
 
 Graph::Graph() {}
+Graph::Graph(int num, char form)
+{
+	n = num;
+	m = 0;
+	graph_form = form;
+	d = 0;
+	w = 1;
+
+	switch (graph_form){
+	case 'C':
+		graph_m.resize(n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				graph_m[i].push_back(0);
+			}
+		}
+		break;
+	case 'L':
+		graph_lw.resize(n);
+		break;
+	case 'E':
+
+		break;
+	}
+}
 
 Graph::~Graph() {}
 
@@ -705,4 +730,345 @@ void Graph::writeGraph(const string file_name)
 				   break;
 		}
 	}
+}
+Graph Graph::getSpaningTreePrima()
+{
+	Graph ng = Graph(n, graph_form);      
+
+	int max_w = 0;
+	if (graph_form == 'C') 
+	{
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = 0; j < n; j++) 
+			{
+				if (graph_m[i][j] > max_w) 
+				{
+					max_w = graph_m[i][j];
+				}
+			}
+		}
+	}
+	else if (graph_form == 'L') 
+	{
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = 0; j < graph_lw[i].size(); j++) 
+			{
+				if (graph_lw[i][j].second > max_w) 
+				{
+					max_w = graph_lw[i][j].second;
+				}
+			}
+		}
+	}
+	else if (graph_form == 'E') {
+		for (int i = 0; i < graph_ew.size(); i++) 
+		{
+			if (get<2>(graph_ew[i]) > max_w) 
+			{
+				max_w = get<2>(graph_ew[i]);
+			}
+		}
+	}
+
+	vector <bool> visited(n, false);     
+	vector <int> min_e(n, max_w + 1);    
+	vector <int> sel_e(n, -1);         
+	set <pair<int, int> > q;
+	int totalMSTSum = 0;             
+
+	if (graph_form == 'C') 
+	{
+		min_e[0] = 0;
+
+		for (int i = 0; i < n; i++) 
+		{
+			int v = -1;
+			for (int j = 0; j < n; j++) 
+			{
+				if (!visited[j] && (v == -1 || min_e[j] < min_e[v])) 
+				{
+					v = j;
+				}
+			}
+
+			visited[v] = true;
+			for (int to = 0; to < n; to++) 
+			{
+				if (!visited[to] && graph_m[v][to] != 0 && graph_m[v][to] < min_e[to]) 
+				{
+					min_e[to] = graph_m[v][to];
+					sel_e[to] = v;
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) 
+		{
+			if (sel_e[i] != -1) 
+			{
+				ng.addEdge(sel_e[i] + 1, i + 1, graph_m[sel_e[i]][i]);
+				totalMSTSum += graph_m[sel_e[i]][i];
+			}
+		}
+	}
+	else if (graph_form == 'L' || graph_form == 'E') 
+	{
+		if (graph_form == 'E') 
+		{
+			this->transformToAdjList();
+		}
+
+		int start = 0;
+		while (start < n) 
+		{
+			min_e[start] = 0;
+			q.insert(make_pair(0, start));
+
+			while (!q.empty()) 
+			{
+				int v = q.begin()->second;
+				q.erase(q.begin());
+				visited[v] = true;
+
+				for (size_t j = 0; j < graph_lw[v].size(); j++) 
+				{
+					int to = graph_lw[v][j].first - 1,
+						cost = graph_lw[v][j].second;
+					if (!visited[to] && cost < min_e[to]) 
+					{
+						q.erase(make_pair(min_e[to], to));
+						min_e[to] = cost;
+						sel_e[to] = v;
+						q.insert(make_pair(min_e[to], to));
+					}
+				}
+			}
+
+			while (start < n && visited[start]) 
+			{
+				start++;
+			}
+		}
+
+		for (int i = 0; i < n; i++) 
+		{
+			if (sel_e[i] != -1) 
+			{
+				ng.addEdge(sel_e[i] + 1, i + 1, min_e[i]);
+				totalMSTSum += min_e[i];
+			}
+		}
+	}
+
+	cout << "Sum: " << totalMSTSum << endl;
+
+	return ng;
+}
+
+bool sortcol(const tuple<int, int, int>& v1,
+	const tuple<int, int, int>& v2) 
+{
+	return get<2>(v1) < get<2>(v2);
+}
+
+Graph Graph::getSpaningTreeKruscal()
+{
+	char transformedFrom = 'E';
+	if (graph_form != 'E') 
+	{
+		transformedFrom = graph_form;
+		this->transformToListOfEdges();
+	}
+
+	Graph ng = Graph(n, transformedFrom);
+	int cost = 0;
+	vector <pair<int, int> > res;
+	DSU dsu;
+
+	sort(graph_ew.begin(), graph_ew.end(), sortcol);
+
+	for (int i = 0; i < m; i++)
+		dsu.MakeSet(i);
+
+	for (int i = 0; i < m; i++) 
+	{
+		int a = get<0>(graph_ew[i]),
+			b = get<1>(graph_ew[i]),
+			w = get<2>(graph_ew[i]);
+
+		if (dsu.Find(a) != dsu.Find(b)) 
+		{
+			cost += w;
+			res.push_back(make_pair(a, b));
+			dsu.Union(a, b);
+			ng.addEdge(a, b, w);
+		}
+	}
+
+	cout << "Krs: " << cost << endl;
+
+	switch (transformedFrom) {
+	case 'C':
+		this->transformToAdjMatrix();
+		break;
+	case 'L':
+		this->transformToAdjList();
+		break;
+	}
+
+	return ng;
+}
+
+Graph Graph::getSpaningTreeBoruvka()
+{
+	Graph ng = Graph(n, graph_form);
+
+	DSU dsu;
+	vector <int> minEdge(n);
+	vector <int> sel_e(n);
+	int cost = 0;
+
+	int INF = 0; 
+	if (graph_form == 'E') {
+		for (int k = 0; k < graph_ew.size(); k++) 
+		{
+			if (get<2>(graph_ew[k]) > INF)
+				INF = get<2>(graph_ew[k]) + 1;
+		}
+	}
+	else if (graph_form == 'L') {
+		for (int k = 0; k < graph_lw.size(); k++) 
+		{
+			for (int j = 0; j < graph_lw[k].size(); j++) 
+			{
+				if (graph_lw[k][j].second > INF)
+					INF = graph_lw[k][j].second + 1;
+			}
+		}
+	}
+	else if (graph_form == 'C') {
+		for (int k = 0; k < graph_m.size(); k++) {
+			for (int j = 0; j < graph_m[k].size(); j++) {
+				if (graph_m[k][j] > INF)
+					INF = graph_m[k][j] + 1;
+			}
+		}
+	}
+
+	int ng_m = 0;
+	while (ng_m < n - 1) 
+	{
+		for (int i = 0; i < minEdge.size(); i++) 
+		{
+			minEdge[i] = INF;
+		}
+
+		for (int i = 0; i < sel_e.size(); i++) 
+		{
+			sel_e[i] = -1;
+		}
+
+		if (graph_form == 'E') 
+		{
+			for (int i = 0; i < graph_ew.size(); i++) 
+			{
+				dsu.MakeSet(get<0>(graph_ew[i]) - 1);
+				dsu.MakeSet(get<1>(graph_ew[i]) - 1);
+				int a = dsu.Find(get<0>(graph_ew[i]) - 1);
+				int b = dsu.Find(get<1>(graph_ew[i]) - 1);
+				int w = get<2>(graph_ew[i]);
+				if (a != b) 
+				{
+					if (minEdge[a] > w) 
+					{
+						minEdge[a] = w;
+						sel_e[a] = b;
+					}
+					if (minEdge[b] > w) 
+					{
+						minEdge[b] = w;
+						sel_e[b] = a;
+					}
+				}
+			}
+		}
+		else if (graph_form == 'L') {
+			for (int i = 0; i < graph_lw.size(); i++) 
+			{
+				for (int j = 0; j < graph_lw[i].size(); j++) 
+				{
+					dsu.MakeSet(i);
+					dsu.MakeSet(graph_lw[i][j].first - 1);
+					int a = dsu.Find(i);
+					int b = dsu.Find(graph_lw[i][j].first - 1);
+					int w = graph_lw[i][j].second;
+					if (a != b) 
+					{
+						if (minEdge[a] > w) 
+						{
+							minEdge[a] = w;
+							sel_e[a] = b;
+						}
+						if (minEdge[b] > w) 
+						{
+							minEdge[b] = w;
+							sel_e[b] = a;
+						}
+					}
+				}
+			}
+		}
+		else if (graph_form == 'C') {
+			for (int i = 0; i < graph_m.size(); i++) 
+			{
+				for (int j = 0; j < graph_m[i].size(); j++) 
+				{
+					dsu.MakeSet(i);
+					dsu.MakeSet(j);
+					int a = dsu.Find(i);
+					int b = dsu.Find(j);
+					int w = graph_m[i][j];
+					if (w != 0 && a != b) 
+					{
+						if (minEdge[a] > w) 
+						{
+							minEdge[a] = w;
+							sel_e[a] = b;
+						}
+						if (minEdge[b] > w) 
+						{
+							minEdge[b] = w;
+							sel_e[b] = a;
+						}
+					}
+				}
+			}
+		}
+
+		bool noEdges = true;
+		for (int i = 0; i < sel_e.size(); i++) 
+		{
+			if (sel_e[i] != -1)
+				noEdges = false;
+		}
+		if (noEdges)
+			break;
+
+		for (int i = 0; i < minEdge.size(); i++) 
+		{
+			if (minEdge[i] != INF && sel_e[i] != -1 && dsu.Find(i) != dsu.Find(sel_e[i])) 
+			{
+				ng.addEdge(i + 1, sel_e[i] + 1, minEdge[i]);
+				cost += minEdge[i];
+				dsu.Union(i, sel_e[i]);
+				ng_m++;
+			}
+		}
+	}
+
+	cout << "Bor: " << cost << endl;
+
+	return ng;
 }
